a403c2a250520d3cb2d005405ae36d190e35bdb6
M1RRoN <mds.myst@gmail.com>
2023-09-19 21:50:01 +0400

    fix test_google_search.py

--- test_google_search.py
+++ test_google_search.py
@@ -2,10 +2,6 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-browser.config.window_height = 1024
-browser.config.window_width = 768
-
-
 def test_google_search_find(window):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
