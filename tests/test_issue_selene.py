6eaf3427aae49652873df9286401c090597dff4e
M1RRoN <mds.myst@gmail.com>
2023-11-18 19:13:34 +0400

    hw9

--- tests/test_issue_selene.py
+++ tests/test_issue_selene.py
@@ -6,5 +6,5 @@ def test_find_issue_with_selene():
     browser.element('#query-builder-test').send_keys('M1RRoN/python_qa').press_enter()
     browser.element(by.link_text('M1RRoN/python_qa')).click()
     browser.element('#issues-tab').click()
-    browser.element(by.partial_text('another test issue')).should(be.visible)
+    browser.element(by.partial_text('test issue')).should(be.visible)
     browser.quit()
